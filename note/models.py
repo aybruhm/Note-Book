cbc091e25f0419b23772af82ac8485a819950eb4
Israel Abraham <israelvictory87@gmail.com>
2020-06-15 14:17:38 +0100

    Created the templates folder, including the base, new, note and index template.

--- note/models.py
+++ note/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 class Note(models.Model):
     title     = models.CharField(max_length=100)
     content   = models.TextField(max_length=600)
-    category  = models.CharField(default=uncategorized)
+    category  = models.CharField(max_length=30)
     timestamp = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
