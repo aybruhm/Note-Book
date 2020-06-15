ec6ce3b39527d25158c2a7e41fd8198391d2632a
Israel Abraham <israelvictory87@gmail.com>
2020-06-15 14:01:35 +0100

    Made migrations to the database.

--- note/models.py
+++ note/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Note(models.Model):
+    title     = models.CharField(max_length=100)
+    content   = models.TextField(max_length=600)
+    category  = models.CharField(default=uncategorized)
+    timestamp = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.title
