bbd8ca4f29732162169bb10a4c74a54341155118
Israel Abraham <israelvictory87@gmail.com>
2020-06-27 12:10:24 +0100

    Modified settings.py file.

--- notebook/settings.py
+++ notebook/settings.py
@@ -126,6 +126,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
